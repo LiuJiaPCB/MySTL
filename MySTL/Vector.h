d18d08f3340be3f562c8a4a5eeed49b7dd8af34f
LiuJia <1550990611@qq.com>
2014-12-08 21:45:32 +0800

    增加vector的迭代器

--- MySTL/Vector.h
+++ MySTL/Vector.h
@@ -3,10 +3,22 @@
 
 namespace MySTL
 {
-	template<class T>
+	template<class T,class Alloc = allocator<T> >
 	class Vector
 	{
-		
+		public:
+			typedef   T              value_type;
+			typedef   value_type*   pointer;
+			typedef   value_type*   iterator;
+			typedef   value_type&   reference;
+			typedef   size_t         size_type;
+			typedef   ptrdiff_t      difference_type;
+			
+		protected:
+		    typedef  Alloc    data_allocator;
+			iterator start;
+			iterator finish;
+			iterator end_of_storage; 
 	};
 }
 
