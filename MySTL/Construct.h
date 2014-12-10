b3b32f32982ddefbf80e57fdb17db8d5e62a9881
LiuJia <1550990611@qq.com>
2014-12-10 22:46:24 +0800

    fix destroy

--- MySTL/Construct.h
+++ MySTL/Construct.h
@@ -12,36 +12,36 @@ namespace MySTL
 	{
 		new(p) T1(value);
 	}
-/*destory*/
+/*destroy*/
 	template <class T>
-	inline void destory(T *pointer)
+	inline void destroy(T *pointer)
 	{
 		pointer->~T();
 	} 
 
 	template<class ForwardIterator>
-	inline void __destory_aux(ForwardIterator first,ForwardIterator last,_false_type)
+	inline void __destroy_aux(ForwardIterator first,ForwardIterator last,_false_type)
 	{
 		for(;first<last;++first)
 		{
-			destory(&*first);
+			destroy(&*first);
 		}
 	}
 	
 	template<class ForwardIterator>
-	inline void __destory_aux(ForwardIterator first,ForwardIterator last,_true_type)
+	inline void __destroy_aux(ForwardIterator first,ForwardIterator last,_true_type)
 	{
 	}
 	
 	template<class ForwardIterator,class T>
-	inline void __destory(ForwardIterator first,ForwardIterator last,T*)
+	inline void __destroy(ForwardIterator first,ForwardIterator last,T*)
 	{
 		typedef typename _type_traits<T>::has_trivial_destructor trivial_destructor;
 		__destroy_aux(first,last,trivial_destructor());
 	}
 	
 	template<class ForwardIterator>
-	inline void destory(ForwardIterator first,ForwardIterator last)
+	inline void destroy(ForwardIterator first,ForwardIterator last)
 	{
 		__destroy(first,last,value_type(first));
 	}	
