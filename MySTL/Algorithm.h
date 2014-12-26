cd20a70e2b0d50b0864fc4cad4078efab62a21d8
LiuJia <1550990611@qq.com>
2014-12-26 23:15:26 +0800

    update Algorithm

--- MySTL/Algorithm.h
+++ MySTL/Algorithm.h
@@ -12,5 +12,75 @@ template<class OutputIterator, class Size, class T>
 		return first;
 	}
 	
+template <class InputIterator,class T>
+T accumulate(InputIterator first,InputIterator last,T init)
+{
+	for(;first!=last;++first)
+	{
+		init=init + (*first);  //累加到初值init身上 
+	}
+	return init;
+} 
+
+template <class InputIterator,class T,class BinaryOperation>
+T accumulate(InputIterator first,InputIterator last,T init,BinaryOperation binary_op)
+{
+	for(;first!=last;++first)
+	{
+		init=binary_op(init,*first);
+	}
+	return init;
+}
+
+template<class InputIterator,class OutputIterator,class T,class BinaryOperation>
+OutputIterator _adjacent_difference(InputIterator first,InputIterator last,OutputIterator result,T*,
+                                   BinaryOperation binary_op)
+{
+	T value = *first;
+	if(++first != last)
+	{
+		T tmp = *first;
+		*++result = binary_op(tmp,value);
+		value = tmp;
+		
+	}				
+	return ++result;	
+}
+template <class InputIterator,class OutputIterator,class BinaryOperation>
+OutputIterator adjacent_differance(InputIterator first,InputIterator last,OutputIterator result,
+                                   BinaryOperation binary_op)
+{
+	if(first == last)
+	{
+		return result;
+	}
+	*result = *first;
+    return 	_adjacent_difference(first,last,result,value_type(first),
+                                 binary_op);			   	
+} 
+
+template<class InputIterator1,class InputIterator2,class T>
+T inner_product(InputIterator1 first1,InputIterator1 last1,InputIterator2 first2,T init)
+{
+	for(;first!=last;++first,++first2)
+	{
+		init = init + (*first1 * *first2);
+	}
+	return init;
+}
+
+template<class InputIterator1,class InputIterator2,class T,
+         class BinaryOperation1,class BinaryOperation2>
+T inner_product(InputIterator1 first1,InputIterator1 last1,InputIterator2 first2,T init,
+                BinaryOperation1 binary_op1,BinaryOperation2 binary_op2)
+{
+	for(;first!=last;++first,++first2)
+	{
+		init = binary_op1(init,binary_op2(*first1,*first2));
+	}
+	return init;
+}
+
+	
 }
 #endif
